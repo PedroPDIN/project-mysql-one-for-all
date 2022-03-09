d179d1b787b55fec8cedae9c61258a82d0ec8803
Pedro Lima <pedro.oficial1969@gmail.com>
2022-03-09 13:07:17 -0300

    refactor

--- desafio1.sql
+++ desafio1.sql
@@ -6,7 +6,7 @@ CREATE TABLE SpotifyClone.plan(
 plan_id INT PRIMARY KEY AUTO_INCREMENT,
 `name` VARCHAR(100) NOT NULL,
 price DECIMAL(3,2) NOT NULL
-)ENGINE=InnoDB;
+)engine=InnoDB;
 
 CREATE TABLE SpotifyClone.`user`(
 user_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -15,12 +15,12 @@ age INT NOT NULL,
 plan_id INT NOT NULL,
 plan_date VARCHAR(100) NOT NULL,
 FOREIGN KEY (plan_id) REFERENCES SpotifyClone.plan(plan_id)
-)ENGINE=InnoDB;
+)engine=InnoDB;
 
 CREATE TABLE SpotifyClone.artist(
 artist_id INT PRIMARY KEY AUTO_INCREMENT,
 artist VARCHAR(50)
-)ENGINE=InnoDB;
+)engine=InnoDB;
 
 CREATE TABLE SpotifyClone.user_artist(
 user_id INT NOT NULL,
@@ -28,13 +28,13 @@ artist_id INT NOT NULL,
 FOREIGN KEY (user_id) REFERENCES `user`(user_id),
 FOREIGN KEY (artist_id) REFERENCES artist(artist_id),
 PRIMARY KEY (user_id, artist_id)
-)ENGINE = InnoDB;
+)engine = InnoDB;
 
 CREATE TABLE SpotifyClone.album(
 album_id INT PRIMARY KEY AUTO_INCREMENT,
 album VARCHAR(50) NOT NULL,
 age INT NOT NULL
-)ENGINE=InnoDB;
+)engine=InnoDB;
 
 CREATE TABLE SpotifyClone.song(
 song_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -44,7 +44,7 @@ artist_id INT NOT NULL,
 duration INT NOT NULL,
 FOREIGN KEY (album_id) REFERENCES SpotifyClone.album(album_id),
 FOREIGN KEY (artist_id) REFERENCES SpotifyClone.artist(artist_id)
-)ENGINE=InnoDB;
+)engine=InnoDB;
 
 CREATE TABLE SpotifyClone.`history`(
 user_id INT NOT NULL,
@@ -53,7 +53,7 @@ song_id INT NOT NULL,
 FOREIGN KEY (user_id) REFERENCES SpotifyClone.`user`(user_id),
 FOREIGN KEY (song_id) REFERENCES SpotifyClone.song(song_id),
 PRIMARY KEY (user_id, song_id)
-)ENGINE=InnoDB;
+)engine=InnoDB;
 
 
 INSERT INTO SpotifyClone.plan
