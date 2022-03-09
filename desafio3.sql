4bf41faf6274420abb69487ae235769e2bb03166
Pedro Lima <pedro.oficial1969@gmail.com>
2022-03-09 15:44:47 -0300

    feat(requisito 3): adiciona requisito 3 completo.

---
+++ desafio3.sql
@@ -0,0 +1,11 @@
+SELECT 
+u.user_name AS usuario,
+COUNT(h.song_id) AS qtde_musicas_ouvidas,
+ROUND(SUM(s.duration / 60), 2) AS total_minutos
+FROM SpotifyClone.`user` AS u 
+INNER JOIN SpotifyClone.`history` AS h
+ON u.user_id = h.user_id
+INNER JOIN SpotifyClone.song AS s
+ON s.song_id = h.song_id
+GROUP BY u.user_id
+ORDER BY u.user_name;
