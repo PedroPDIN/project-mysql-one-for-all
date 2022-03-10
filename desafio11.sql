bbee2403178833ede457333af3fe104a997c326a
Pedro Lima <pedro.oficial1969@gmail.com>
2022-03-09 21:33:33 -0300

    refactor: adiciona alteração do requisito 11.

--- desafio11.sql
+++ desafio11.sql
@@ -3,14 +3,16 @@ s.song AS nome_musica,
 CASE
 WHEN s.song LIKE '%Streets' THEN REPLACE(s.song, 'Streets', 'Code Review')
 WHEN s.song LIKE '%Her Own' THEN REPLACE(s.song, 'Her Own', 'Trybe')
+WHEN s.song LIKE '%Inner Fire' THEN REPLACE(s.song, 'Inner Fire', 'Project')
 WHEN s.song LIKE '%Silly' THEN REPLACE(s.song, 'Silly', 'Nice')
 WHEN s.song LIKE '%Circus' THEN REPLACE(s.song, 'Circus', 'Pull Request')
 ELSE s.song
-END AS novo_nome
+END AS nova_nome
 FROM SpotifyClone.song AS s
 WHERE 
 s.song LIKE '%Streets' OR
 s.song LIKE '%Her Own'OR
+s.song LIKE '%Inner Fire'OR
 s.song LIKE '%Silly' OR
 s.song LIKE '%Circus'
 ORDER BY nome_musica;
